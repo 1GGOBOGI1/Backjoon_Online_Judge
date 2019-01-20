bb37860ebd739f3d3f3e6ff7e2a50fbd02a73e24
JuHui_Kim <kjh107704@gmail.com>
2019-01-20 22:06:48 +0900

    [BOJ] 11721

--- 11721.cpp
+++ 11721.cpp
@@ -4,21 +4,22 @@ using namespace std;
 
 int main()
 {
-    string input;
-    cin >> input;
-    for (int i = 0; i < input.size();)
-    {
-        if (i + 10 < input.size())
-        {
-            string output = input.substr(i, 10);
-            cout << output << endl;
-            i += 10;
-        }
-        else
-        {
-            string output = input.substr(i);
-            cout << output << endl;
-            return 0;
-        }
-    }
+    // string input;
+    // cin >> input;
+    // for (int i = 0; i < input.size();)
+    // {
+    //     if (i + 10 < input.size())
+    //     {
+    //         string output = input.substr(i, 10);
+    //         cout << output << endl;
+    //         i += 10;
+    //     }
+    //     else
+    //     {
+    //         string output = input.substr(i);
+    //         cout << output << endl;
+    //         return 0;
+    //     }
+    // }
+    cout << "helloworld" << endl;
 }
