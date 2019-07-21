71208d49e3a4c1f4560d0922fc51c16a567c492c
Kim Juhui <kjh107704@gmail.com>
2019-07-21 23:01:43 +0900

    [BOJ] 1992

--- 1992.cpp
+++ 1992.cpp
@@ -1,50 +1,50 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
-#include <cmath>
+#include <string>
 using namespace std;
 
-int img[64][64] = {0};
-void zip(int x, int y, int size)
+void quadTree(vector<vector<int>> &pic, int size, int x, int y, int original_size)
 {
-    cout << "(";
+
+    int standard = pic[x][y];
+
     if (size == 1)
     {
-        cout << img[x][y];
+        cout << pic[x][y];
         return;
     }
-    else
-    {
-        int base = img[x][y];
-        bool check = true;
-        for (int i = 0; i < x + size; i++)
-            for (int j = 0; j < y + size; j++)
-                if (img[i][j] != base)
-                {
-                    check = false;
-                }
-        if (!check)
-        {
-            zip(x, y, size / 2);
-            zip(x + size / 2, y, size / 2);
-            zip(x + size / 2, y, size / 2);
-            zip(x + size / 2, y + size / 2, size / 2);
-        }
-        else
-        {
-            cout << base;
-            return;
-        }
-    }
-    cout << ")";
+
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            if (pic[x + i][y + j] != standard)
+            {
+                cout << "(";
+                quadTree(pic, size / 2, x, y, original_size);
+                quadTree(pic, size / 2, x, y + (size / 2), original_size);
+                quadTree(pic, size / 2, x + (size / 2), y, original_size);
+                quadTree(pic, size / 2, x + (size / 2), y + (size / 2), original_size);
+                cout << ")";
+                return;
+            }
+
+    cout << standard;
+    return;
 }
 
 int main()
 {
     int N;
     cin >> N;
+
+    vector<vector<int>> pic(N, vector<int>(N));
+
     for (int i = 0; i < N; i++)
+    {
+        string input;
+        cin >> input;
         for (int j = 0; j < N; j++)
-            scanf("%d", &img[i][j]);
-    zip(0, 0, N);
+            pic[i][j] = int(input[j] - 48);
+    }
+
+    quadTree(pic, N, 0, 0,N);
 }
