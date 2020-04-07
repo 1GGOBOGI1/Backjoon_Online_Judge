449e928110826d6f6fcb05ab7198bbfd9b7246fd
Kim Juhui <kjh107704@gmail.com>
2020-04-07 13:57:36 +0900

    BFS

---
+++ BOJ/11725.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void makeGraph(vector<int> *graph, int E)
+{
+    while(E--)
+    {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+}
+
+void BFS(vector<int> *graph, int V, int *parent)
+{
+    queue<int> q;
+    bool check[V + 1] = {false};
+
+    q.push(1);
+
+    while(!q.empty())
+    {
+        int now = q.front();
+        check[now] = true;
+        q.pop();
+
+        for (int i = 0; i < graph[now].size();i++)
+        {
+            int next = graph[now][i];
+            if(!check[next])
+            {
+                parent[next] = now;
+                q.push(next);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<int> graph[N + 1];
+    int parent[N + 1];
+
+    makeGraph(graph, N-1);
+    BFS(graph, N, parent);
+
+    for (int i = 2; i <= N;i++)
+        cout << parent[i] << "\n";
+}
