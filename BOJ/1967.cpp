7d0d7cc6f0c3ee07648cbcbb45bd930e81a8776b
Kim Juhui <kjh107704@gmail.com>
2020-04-10 22:25:46 +0900

    tree diameter

--- BOJ/1967.cpp
+++ BOJ/1967.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -65,5 +66,74 @@ int main()
     vector<pair<int,int>> node[n + 1];
     makeGraph(node, n - 1);
 
+    cout << getDiameter(node, n);
+}
+*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+pair<int,int> BFS(vector<pair<int,int>> *node, int V, int start)
+{
+    queue<int> q;
+    bool check[V + 1] = {false};
+    int distance[V + 1] = {0};
+
+    distance[start] = 0;
+    check[start] = true;
+    q.push(start);
+
+    while(!q.empty())
+    {
+        int now = q.front();
+        q.pop();
+
+        for (int i = 0; i < node[now].size();i++)
+        {
+            int next = node[now][i].first;
+            if(!check[next])
+            {
+                distance[next] = distance[now] + node[now][i].second;
+                check[next] = true;
+                q.push(next);
+            }
+        }
+    }
+
+    int maxDistanceIndex = max_element(distance, distance + V + 1) - distance;
+
+    return make_pair(maxDistanceIndex, distance[maxDistanceIndex]);
+}
+
+void makeGrpah(vector<pair<int,int>> *node, int V)
+{
+    int E = V - 1;
+    while(E--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        node[a].push_back(make_pair(b, c));
+        node[b].push_back(make_pair(a, c));
+    }
+}
+
+int getDiameter(vector<pair<int,int>> *node, int V)
+{
+    pair<int, int> ans = BFS(node, V, 1);
+    ans = BFS(node, V, ans.first);
+
+    return ans.second;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<pair<int,int>> node[n + 1];
+    makeGrpah(node, n);
     cout << getDiameter(node, n);
 }
