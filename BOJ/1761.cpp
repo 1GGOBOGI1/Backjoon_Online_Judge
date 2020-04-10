245fb42b1663a1045b4d6c9cb89d46aab4edd89d
Kim Juhui <kjh107704@gmail.com>
2020-04-10 22:25:35 +0900

    LCA

--- BOJ/1761.cpp
+++ BOJ/1761.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -103,5 +104,103 @@ int main()
         int u, v;
         cin >> u >> v;
         cout << LCA(depth, parent, u, v)<<"\n";
+    }
+}
+*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void BFS(vector<pair<int,int>> *node, int V, int *p, int * distance, int *depth)
+{
+    queue<int> q;
+    bool check[V + 1] = {false};
+
+    p[1] = 0;
+    distance[1] = 0;
+    depth[1] = 0;
+    check[1] = true;
+    q.push(1);
+
+    while(!q.empty())
+    {
+        int now = q.front();
+        q.pop();
+
+        for (int i = 0; i < node[now].size();i++)
+        {
+            int next = node[now][i].first;
+            if(!check[next])
+            {
+                p[next] = now;
+                distance[next] = node[now][i].second;
+                depth[next] = depth[now] + 1;
+                check[next] = true;
+                q.push(next);
+            }
+        }
+    }
+}
+
+void makeGraph(vector<pair<int,int>> *node, int *p, int * distance, int *depth,int V)
+{
+    int E = V - 1;
+    while(E--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        node[a].push_back(make_pair(b, c));
+        node[b].push_back(make_pair(a, c));
+    }
+    BFS(node, V, p, distance, depth);
+}
+
+int LCA(int *parent, int *distance, int *depth,int u, int v)
+{
+    int nodeDistance = 0;
+    if(depth[u] < depth[v])
+        swap(u, v);
+
+    // depth[u] > depth[v]
+
+    while(depth[u] !=  depth[v])
+    {
+        nodeDistance += distance[u];
+        u = parent[u];
+    }
+
+    while(u != v)
+    {
+        nodeDistance += distance[u];
+        u = parent[u];
+        nodeDistance += distance[v];
+        v = parent[v];
+    }
+
+    return nodeDistance;
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<pair<int,int>> node[N + 1];
+    int depth[N + 1];
+    int parent[N + 1];
+    int distanceToParent[N + 1];
+    
+    makeGraph(node, parent, distanceToParent, depth, N);
+
+    int M;
+    cin >> M;
+    while(M--)
+    {
+        int u, v;
+        cin >> u >> v;
+        cout << LCA(parent, distanceToParent, depth, u, v)<<"\n";
     }
 }
