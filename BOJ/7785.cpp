2013b8b1f493874bf0fdb8f5ae6b7bd985d23d49
Kim Juhui <kjh107704@gmail.com>
2020-04-24 13:37:50 +0900

    set을 이용한 BST

--- BOJ/7785.cpp
+++ BOJ/7785.cpp
@@ -1,54 +1,25 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-
+#include <set>
 using namespace std;
 
 int main()
 {
-    const size_t size{26};
-    vector<vector<string> > log;
-    unsigned int n;
-    cin >> n;
+    int n;
+    scanf("%d", &n);
+
+    set<string> office;
+
+    string name, state;
     while (n--)
     {
-        string name, condition;
-        cin >> name >> condition;
+        cin >> name >> state;
 
-        int point = name[0] - 65;
-        if (!(0 <= point && point < 91))
-        {
-            point = point - 32;
-        }
-
-        if (condition == "enter")
-        {
-            log[point].push_back(name);
-        }
-        else
-        {
-            vector<string>::iterator iter = find(log[point].begin(), log[point].end(), name);
-
-            if (iter != log[point].end())
-            {
-                log[point].erase(iter);
-            }
-        }
+        if(state == "enter")
+            office.insert(name);
+        else if(state == "leave")
+            office.erase(office.find(name));
     }
 
-    for (int i = 0; i < 26; i++)
-    {
-        sort(log[i].begin(), log[i].end());
-    }
-
-    for (int i = 0; i < 26; i++)
-    {
-        while (log[i].size())
-        {
-            cout << log[i].back() << endl;
-            log[i].pop_back();
-        }
-    }
-    return 0;
+    for (auto iter = office.rbegin(); iter != office.rend(); iter++)
+        cout << *iter << "\n";
 }
