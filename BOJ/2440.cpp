5b8571660205cd5ce75ac40d314715fd9ead221c
Kim Juhui <kjh107704@gmail.com>
2020-05-12 23:58:48 +0900

    단순 별찍기

---
+++ BOJ/2440.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    for (int i = N; i > 0; i--)
+    {
+        for (int j = i; j > 0; j--)
+            printf("*");
+        printf("\n");
+    }
+}
