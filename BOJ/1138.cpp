78d6b91ffdecbbb56db07bbd7faaa6438c494390
Kim Juhui <kjh107704@gmail.com>
2020-05-12 23:52:19 +0900

    그리디 알고리즘

--- BOJ/1138.cpp
+++ BOJ/1138.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -18,4 +19,36 @@ int main()
     }
     for (int i = 0; i < N; i++)
         cout << ANS[i] << " ";
+}
+*/
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int people[N + 1] = {0};
+    int answer[N + 1] = {0};
+
+    for (int i = 1; i <= N; i ++)
+        scanf("%d", &people[i]);
+
+    for (int i = 1; i <= N; i++)
+    {
+        int cnt = 0, j = 1;
+
+        for (; cnt != people[i]+1; j++)
+        {
+            if(answer[j] == 0)
+                cnt++;
+        }
+        answer[j-1] = i;
+    }
+
+    for (int i = 1; i <= N; i++)
+        printf("%d ", answer[i]);
 }
